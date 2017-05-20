6af19fb8d0b36518a919f336c09afe351219a3e9
brolin <brolin108@gmail.com>
2017-05-20 12:57:26 -0500

    Actualiza datos

--- shinyViz/ui.R
+++ shinyViz/ui.R
@@ -3,17 +3,17 @@ library(leaflet)
 
 ## Define UI for dataset viewer application
 ui <- function(request) {
-  shinyUI(fluidPage(
+  fluidPage(
     titlePanel("Reporte de servicios por municipio"),
     sidebarLayout(
       sidebarPanel(
 
         dateRangeInput("dates", start = "2015-01-01", end = "2016-12-31", label = h3("Rango de fechas")),
 
-      ),
+        ),
       mainPanel(
         leafletOutput("map")
       )
     )
-  ))
+  )
 }
