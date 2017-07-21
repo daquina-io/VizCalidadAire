7bea407b91fb3575f9e5ffbfd57306a61b094279
son0p <fede2001@gmail.com>
2017-07-21 18:40:34 -0500

    restringe puntos al inicio para disminuir problema de rendimiento

--- ui.R
+++ ui.R
@@ -10,8 +10,8 @@ ui <- function(request) {
     leafletOutput("map", width = "100%", height = "100%"),
     absolutePanel(top = 10, right = 10,
                   titlePanel("Contaminación del aire en Medellín"),
-                  sliderInput("range", "Concentración partículas PM2.5 (ug/m3)", 0, 500,value = c(0,100), step = 1 ),
-                  dateRangeInput("dates", start = "2017-01-01", end = "2018-01-01", label = h3("Rango de fechas")),
+                  sliderInput("range", "Concentración partículas PM2.5 (ug/m3)", 0, 500,value = c(80,100), step = 1 ),
+                  dateRangeInput("dates", start = "2017-06-01", end = "2018-01-01", label = h3("Rango de fechas")),
                   selectInput("wday", "Día de la semana", c("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"), multiple=TRUE, selectize=TRUE, selected = c("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday")),
                   bookmarkButton()
                   )
