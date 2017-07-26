98d3df70f315a018262d096c28a4745fdf9dd66d
son0p <fede2001@gmail.com>
2017-07-26 06:41:03 -0500

    agrega fecha en el popup

--- server.R
+++ server.R
@@ -24,9 +24,9 @@ points <- points[points$pm25 < 500,]
 
 ## colors
 points$colors <- lapply(points$pm25, function(x)(
-  ifelse(x < 30 , "green",
-    ifelse(x < 50, "yellow",
-      ifelse( x < 100, "red","purple")))
+  ifelse(x < 12 , "green",
+    ifelse(x < 35 , "yellow",
+      ifelse( x < 55, "red","purple")))
 ))
 
 shinyServer(function(input, output) {
@@ -45,6 +45,6 @@ shinyServer(function(input, output) {
     leafletProxy("map", data = data()) %>%
       clearShapes() %>%
       ## addCircles(~as.numeric(lng), ~as.numeric(lat), popup = ~as.character(pm25), fillOpacity = 0.7, radius = ~as.numeric(pm25)) ## no colors
-      addCircles(~as.numeric(lng), ~as.numeric(lat), popup = ~as.character(pm25), fillOpacity = 0.7, radius = 10, color = ~colors)
+      addCircles(~as.numeric(lng), ~as.numeric(lat), popup = paste(pm25,date_hour), fillOpacity = 0.7, radius = 10, color = ~colors)
   })
 })
