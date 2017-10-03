e5614af13b0c8d41f8c70d764194cab88453fc9a
son0p <fede2001@gmail.com>
2017-10-03 16:51:14 -0500

    no habíamos logrado un prototipo de mapa con promedios por zonas

---
+++ hexBin/hexBin_test.R
@@ -0,0 +1,22 @@
+
+
+require(readr)
+require(lubridate)
+require(rbokeh)
+points <- read_csv("https://github.com/daquina-io/VizCalidadAire/raw/master/data/points.csv")
+
+filterPoints <- function(df,col, maxValue, minValue){
+ (df[,col] > minValue & df[,col] < maxValue)
+}
+
+points_fltr<- points[filterPoints(points, "lat", 6.6, 5.3) & filterPoints(points, "lng", -76, -74),]
+
+
+## hexbin con mapa de fondo
+gmap(lat = 6.25, lng = -75.57, zoom = 13, width = 700, height = 600) %>%
+  ly_hexbin(as.numeric(points_fltr$lng), as.numeric(points_fltr$lat), as.numeric(points_fltr$pm25), alpha =0.5, xbins = 200)
+
+## hexbin sin mapa
+figure(width = 800, height = 450, padding_factor = 0) %>%
+  ly_map("world", col = "gray") %>%
+  ly_hexbin(as.numeric(points_fltr$lng), as.numeric(points_fltr$lat), as.numeric(points_fltr$pm25))
