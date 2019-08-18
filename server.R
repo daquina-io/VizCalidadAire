e602791aff2f4451a53b89ecf5a689b3eb840f1d
brolin <brolin108@gmail.com>
2019-08-17 19:51:23 -0500

    Agrega la posibilidad de ir a consultar las mediciones del sensor en la última hora al panel de chronograf

--- server.R
+++ server.R
@@ -11,52 +11,43 @@ if(!require(lubridate)) install.packages('lubridate')
 if(!require(influxdbr)) install.packages('influxdbr')
 
 ## conexión remota
-host <- "gblabs.co"
-        ## "aqa.unloquer.org"
+host <- ##"gblabs.co"
+        "aqa.unloquer.org"
         ## "aireciudadano.servehttp.com"
-db <-   "canairio"
-        ## "aqa"
+db <-   ## "canairio"
+        "aqa"
         ## "ENVdataDB"
 con <- influx_connection(scheme = c("http", "https"), host = host,port = 8086, group = NULL, verbose = FALSE, config_file = "~/.influxdb.cnf")
-
-## dummy data to initialize dataframe and to create empty dataframe
-sensorDummy <-  matrix(nrow = 0, ncol = 3)
-colnames(sensorDummy) <- c("pm25", "lat", "lng")
-
-## get data from influxdb API
-db.query <- function(sensorName, time){
-  x <- tryCatch({
-    sensorData <- influx_query(con, db = db, query = sprintf("SELECT mean(\"pm25\") AS \"pm25\", median(\"lat\") AS \"lat\", median(\"lng\") AS \"lng\" FROM %s.\"autogen\".\"\"%s\"\" WHERE time > now() - %dm GROUP BY time(2s) FILL(none) LIMIT 150",db,sensorName,time),timestamp_format = c("n", "u", "ms", "s", "m", "h"))
-    as.data.frame(sensorData)},
-    error = function(error_message) {
-      message(sprintf("error en %s",sensorName))
-      return(as.data.frame(sensorDummy))
-    })
-}
-
-points <- function(sensorName, time){
-  df <- rbind(
-    db.query(sensorName, time)
-  )
-  df$colors <- lapply(df$pm25, function(x)(
-    ifelse(x < 12 , "green",
-    ifelse(x < 35 && x >= 12 , "gold",
-    ifelse( x < 55 && x >= 35, "orange",
-    ifelse( x < 150 && x >= 55, "red",
-    ifelse( x < 250 && x >= 150, "purple",
-           "maroon")))))
-  ))
-   return(df)
-}
 
 measurements <- unlist( show_measurements(con = con,
                                           db = db
                                           ))
 
-sensores <- paste0("\"",db,"\"",".autogen.","\"",measurements,"\"",collapse=",")
+sensores <- function(db, measurements) {
+    paste0("\"",db,"\"",".autogen.","\"",measurements,"\"",collapse=",")
+}
 
+sensores_ts <- function(db, measurements) {
+    map(measurements, function(ms) paste0("\"",db,"\"",".autogen.","\"",ms,"\"",collapse=","))
+}
 
-data <- influx_query(con, db = db, query = sprintf("SELECT mean(\"pm25\") AS \"pm25\", median(\"lat\") AS \"lat\", median(\"lng\") AS \"lng\" FROM %s WHERE time > now() - 1h GROUP BY time(1h) FILL(none) LIMIT 1",sensores),timestamp_format = c("n", "u", "ms", "s", "m", "h"))
+## "aqa"."autogen"."volker0004"
+query_time_series <- "SELECT mean(\"pm25\") AS \"pm25\" FROM %s WHERE time > now() - 1h GROUP BY time(10s) FILL(none)"
+query_1h_mean <- "SELECT mean(\"pm25\") AS \"pm25\", median(\"lat\") AS \"lat\", median(\"lng\") AS \"lng\" FROM %s WHERE time > now() - 1h GROUP BY time(1h) FILL(none) LIMIT 1"
+
+query <- function(snsrs, type = "ts") {
+    if(type == "1h") sprintf(query_1h_mean, snsrs)
+    else sprintf(query_time_series, snsrs)
+}
+
+popup_content <- function(query_str, sensorname) {
+        paste0(
+            "Click para ver mediciones de la última<br/>",
+            sprintf("hora del sensor <b><a target='_blank' href='http://%s:8888/sources/1/chronograf/data-explorer?query=%s'>%s</a></b>",host, query_str, sensorname)
+              )
+}
+
+data <- influx_query(con, db = db, query = query(sensores(db,measurements),"1h"),timestamp_format = c("n", "u", "ms", "s", "m", "h"))
 
 x <- as.data.frame(matrix(unlist(data), ncol=3, byrow = TRUE))
 
@@ -100,7 +91,7 @@ x <- x[!is.na(x$lng),]
 
 ## No funciona encuadre
 encuadre <- function(servidor) {
-    if(servidor == "glabs.co") return(fitBounds(-74.079,4.46,-74.065, 4.823))##Bogotá
+    if(servidor != "gblabs.co") return(fitBounds(-74.079,4.46,-74.065, 4.823))##Bogotá
     fitBounds(-75.5, 6.16, -75.57, 6.35)#Medellín
 }
 
@@ -112,11 +103,14 @@ shinyServer(function(input, output) {
 
   output$map <- renderLeaflet({
       leaflet() %>%
-          addProviderTiles(providers$Stamen.Terrain, options = providerTileOptions(noWrap = TRUE) ) %>%
-          fitBounds(-74.079,4.46,-74.065, 4.823) ## la candelaria Bogota
-      ##  ## medellin/test
+          addProviderTiles(providers$CartoDB.DarkMatter, options = providerTileOptions(noWrap = TRUE) ) %>%
+          ## addProviderTiles(providers$Stamen.Terrain, options = providerTileOptions(noWrap = TRUE) ) %>%
+          ## fitBounds(-74.079,4.46,-74.065, 4.823) ## Bogotá
+          fitBounds(-75.5, 6.16, -75.57, 6.35) ## Medellin
   })
 
   leafletProxy("map", data = x )  %>%
-      addCircles( ~as.numeric(lng), ~as.numeric(lat), popup = ~sensorName, opacity= 1.0, fillOpacity = 1.0, radius = 30, fillColor= ~color, color = ~color,  weight = 30, label = ~as.character(as.integer(pm25)), labelOptions = labelOptions(noHide = TRUE, offset=c(0,25), textOnly = TRUE, direction = "top", style = list("color" = "white", "font-weight" = "bold", "font-size" = "16px")))
+      addCircles( ~as.numeric(lng), ~as.numeric(lat),
+                 popup = ~popup_content(query(sensores_ts(db, x$sensorName)), x$sensorName),
+                 opacity= 0.9, fillOpacity = 0.9, radius = 30, fillColor= ~color, color = ~color,  weight = 20, label = ~as.character(as.integer(pm25)), labelOptions = labelOptions(noHide = TRUE, offset=c(0,22), textOnly = TRUE, direction = "top", style = list("color" = "white", "font-weight" = "bold", "font-size" = "12px")))
 })
